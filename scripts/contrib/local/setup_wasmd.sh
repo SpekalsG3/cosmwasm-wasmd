8305c459658f3d74bafaafeeb9d5a9e5993742ae
SpekalsG3 <karpick.kirill@gmail.com>
2024-11-01 02:46:05 +0300

    fix(scripts): generalize setup scripts

--- scripts/contrib/local/setup_wasmd.sh
+++ scripts/contrib/local/setup_wasmd.sh
@@ -6,23 +6,26 @@ STAKE=${STAKE_TOKEN:-ustake}
 FEE=${FEE_TOKEN:-ucosm}
 CHAIN_ID=${CHAIN_ID:-testing}
 MONIKER=${MONIKER:-node001}
+BINARY=${BINARY:-wasmd}
 
-wasmd init --chain-id "$CHAIN_ID" "$MONIKER"
+echo "Using binary: '${BINARY}'"
+
+$BINARY init --chain-id "$CHAIN_ID" "$MONIKER"
 # staking/governance token is hardcoded in config, change this
 ## OSX requires: -i.
 sed -i. "s/\"stake\"/\"$STAKE\"/" "$HOME"/.wasmd/config/genesis.json
-if ! wasmd keys show validator --keyring-backend=test; then
+if ! $BINARY keys show validator --keyring-backend=test; then
   (
     echo "$PASSWORD"
     echo "$PASSWORD"
-  ) | wasmd keys add validator --keyring-backend=test
+  ) | $BINARY keys add validator --keyring-backend=test
 fi
 # hardcode the validator account for this instance
-echo "$PASSWORD" | wasmd genesis add-genesis-account validator "1000000000000$STAKE,1000000000000$FEE" --keyring-backend=test
+echo "$PASSWORD" | $BINARY genesis add-genesis-account validator "1000000000000$STAKE,1000000000000$FEE" --keyring-backend=test
 # (optionally) add a few more genesis accounts
 for addr in "$@"; do
   echo "$addr"
-  wasmd genesis add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE" --keyring-backend=test
+  $BINARY genesis add-genesis-account "$addr" "1000000000$STAKE,1000000000$FEE" --keyring-backend=test
 done
 # submit a genesis validator tx
 ## Workraround for https://github.com/cosmos/cosmos-sdk/issues/8251
@@ -30,7 +33,7 @@ done
   echo "$PASSWORD"
   echo "$PASSWORD"
   echo "$PASSWORD"
-) | wasmd genesis gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID" --amount="250000000$STAKE" --keyring-backend=test
+) | $BINARY genesis gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID" --amount="250000000$STAKE" --keyring-backend=test
 ## should be:
-# (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | wasmd gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID"
-wasmd genesis collect-gentxs
+# (echo "$PASSWORD"; echo "$PASSWORD"; echo "$PASSWORD") | $BINARY gentx validator "250000000$STAKE" --chain-id="$CHAIN_ID"
+$BINARY genesis collect-gentxs
--- scripts/contrib/local/start_node.sh
+++ scripts/contrib/local/start_node.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -eu
 
-wasmd start --rpc.laddr tcp://0.0.0.0:26657 --log_level=info  --trace #remove trace flag if you don't wantg the stack trace to be printed
+BINARY=${BINARY:-wasmd}
+
+$BINARY start --rpc.laddr tcp://0.0.0.0:26657 --log_level=info  --trace #remove trace flag if you don't wantg the stack trace to be printed
